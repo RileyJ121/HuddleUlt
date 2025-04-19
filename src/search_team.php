f24eb53282705c6e3503caa631616f25b4508069
Riley Johnson <71415204+RileyJ121@users.noreply.github.com>
2025-04-19 11:15:34 -0500

    Fixed user profile, added league searching

--- src/search_team.php
+++ src/search_team.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html>
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,37 +8,40 @@
   <link href="../public/main.css" rel="stylesheet">
   <title>Huddle Ult</title>
 </head>
+
 <body>
   <nav>
     <img src="../public/Logo.png" alt="Logo">
     <ul>
-        <li><a href="home.html">Home</a></li>
-        <li><a href="search_team.php">Search</a></li>
-        <li><a href="player_activity.html">Activity</a></li>
-        <li><a href="user_profile.php">Profile</a></li>
+      <li><a href="home.html">Home</a></li>
+      <li><a href="search_team.php">Search</a></li>
+      <li><a href="player_activity.html">Activity</a></li>
+      <li><a href="user_profile.php">Profile</a></li>
     </ul>
   </nav>
   <div class="bg">
     <main>
       <div class="header">
-        <a href="home.html"><h3>🢀</h3></a>
+        <a href="home.html">
+          <h3>🢀</h3>
+        </a>
         <h1>Find Teams</h1>
       </div>
       <div class="main">
-      <?php 
+        <?php
         echo isset($_COOKIE["user"]) ? $_COOKIE["user"] : "Not Logged In";
         echo "<br>";
-      ?>
+        ?>
 
-      <a href="create_club_team.html">New Club Team</a>
-          <a href="create_league_team.html">New League Team</a>
-          <a href="create_pickup_team.html">New Pickup Team</a>
+        <a href="create_club_team.html">New Club Team</a>
+        <a href="create_league_team.html">New League Team</a>
+        <a href="create_pickup_team.html">New Pickup Team</a>
         <form method="get">
           <label for="lat">Latitude:</label>
           <input type="text" id="lat" name="lat" value="">
           <label for="long">Longitude:</label>
           <input type="text" id="long" name="long" value=""><br>
-          <input type="radio" id="team" name="team" value="0" checked="checked" >
+          <input type="radio" id="team" name="team" value="0" checked="checked">
           <label for="pickup"> Any Team</label>
           <input type="radio" id="team" name="team" value="1">
           <label for="pickup"> Pickup Team</label>
@@ -47,36 +51,37 @@
           <label for="league"> Club Team</label>
           <input type="submit" value="Submit">
         </form>
-      <?php
+        <?php
         include 'start_db.php';
         $lat = isset($_GET['lat']) ? $_GET["lat"] : "";
         $long = isset($_GET['long']) ? $_GET["long"] : "";
         $team = isset($_GET['team']) ? $_GET["team"] : 0;
-        
+
         $sql = "SELECT * FROM team WHERE 1 = 1";
 
-        if($long && $lat) {
+        if ($long && $lat) {
           $sql = $sql . " AND ABS(longitude - $long) < 0.6 AND ABS(latitude - $lat) < 0.6";
         }
 
-        if($team > 0) {
+        if ($team > 0) {
           $teamList = array("pickupteam", "leagueteam", "clubteam");
           $sql = $sql . " AND TeamID IN (SELECT TeamID FROM " . $teamList[$team - 1] . ")";
         }
 
-        $result = $conn -> query($sql);
+        $result = $conn->query($sql);
 
-        if($result->num_rows > 0) {
-          while($row = $result -> fetch_assoc()) {
+        if ($result->num_rows > 0) {
+          while ($row = $result->fetch_assoc()) {
             echo $row["Name"] . " (Host: " . $row["Host"] . ")<br>" . $row["TeamDesc"] . "<br><br>";
           }
         } else {
           echo "0 Teams";
         }
-        $conn -> close();
-      ?>
+        $conn->close();
+        ?>
       </div>
     </main>
   </div>
 </body>
+
 </html>
